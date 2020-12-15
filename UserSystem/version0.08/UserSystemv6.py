9809bba8a1a010d3649e7688b630ca76c7a439b9
Alexandros Mikelis <62079690+AlexandrosMikelis@users.noreply.github.com>
2020-12-15 16:57:26 +0200

    Update UserSystemv6.py

--- UserSystem/version0.08/UserSystemv6.py
+++ UserSystem/version0.08/UserSystemv6.py
@@ -55,17 +55,22 @@ class Login(Validate):
         newDB = []
         self.password = newPassword
         UserDB = open("UserDB.txt" , "r+")
+        
         for Userss in UserDB :
             info = Userss.split(" ")
+            
             if(info[0]!=self.email):
                 newDB.append(Userss)
+                
             else:
+                
                 if(info[-1][:-1] == self.nickname or info[-2] == self.surname):
                     newDB.append(self.email + " " 
                                  + newPassword + " " 
                                  + self.name + " "
                                  + self.surname + " " 
                                  + self.nickname + "\n")
+                    
                 else:
                     newDB.append(self.email + " " 
                                  + newPassword + " " 
@@ -76,9 +81,11 @@ class Login(Validate):
                                  + info[-1][:-1] + '\n')
                     
         UserDB.seek(0)
+        
         for i in newDB:
             UserDB.write(i)
         UserDB.close()
+        
         return "Your Successfully Changed Your Password"
     
     def Delete(self):
@@ -89,6 +96,7 @@ class Login(Validate):
                 if self.email not in line:
                     f.write(line)
             f.truncate()
+            
         self.IsLoggedIn=0
         self.IsRegistered =0
         self.logpathcheck = 0
@@ -96,11 +104,14 @@ class Login(Validate):
     def LogIn(self):
         if( self.IsLoggedIn == 0 ):
             self.IsLoggedIn = 1
+            
             if (self.IsRegistered == 1):
                 return "Wrong Password Try Again"
+            
             else:
                 self.IsLoggedIn = 0
                 return "\nYou need to Register First\n"
+            
         else:
             self.logpathcheck =1
             return "\nYou logged in Successfully\n"
