7f25eee6e9a3a536ddbded2838ab1d3cd3339c96
Alexandros Mikelis <62079690+AlexandrosMikelis@users.noreply.github.com>
2020-12-15 16:41:46 +0200

    Update Decorators.py

--- UserSystem/version0.08/Decorators.py
+++ UserSystem/version0.08/Decorators.py
@@ -1,8 +1,9 @@
 import time
 import datetime
 
+#Decorators for UserSystem
 
-
+#Display User Info
 def newChars(func):
     def inner(*args,**kwargs):
         
@@ -16,7 +17,8 @@ def newChars(func):
     return inner
 
 
-
+#Tracking User Session Time and print it out
+#Saving the User Last Time he was online with date - time format on the end of his row in DB
 def timeit(func):
     def timed(*args, **kw):
         
@@ -27,10 +29,13 @@ def timeit(func):
         
         newDB = []
         UserDB = open("UserDB.txt" , "r+")
+        
         for Userss in UserDB :
             info = Userss.split(" ")
+            
             if(info[0]!=args[0].email):
                 newDB.append(Userss)
+                
             else:
                 newDB.append(args[0].email + " " + 
                              args[0].password + " " + 
@@ -40,20 +45,24 @@ def timeit(func):
                              str(datetime.datetime.now()) +'\n')
                 
         UserDB.seek(0)
+        
         for i in newDB:
             UserDB.write(i)
+            
         UserDB.close()
         
         return func(*args, **kw)
     return timed
 
 
-
+#Display Users when an Admin of Staff ask to 
 def UserShowcase(func):
     def UserShow(*args, **kw):
         
         UserDB = open("UserDB.txt" , "r")
+        
         for Users in UserDB :
             print(Users)
+            
         return "Complete"    
     return UserShow
